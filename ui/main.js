a16d2bd73fca14f53ab64a1326954785e8ca9594
Himanshu Khudania <hkhudania@gmail.com>
2017-02-22 11:13:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,13 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 
 element.innerHTML='New Value';
+
+//Move Image 
+
+var img=document.getElementById('madi');
+
+img.onclick=function(){
+    
+    img.style.marginleft='100px';
+    
+};
